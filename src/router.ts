9e36f8a16b1c6aacd145f9c12c3c4247237c7595
LANDAN <landanhsu@bpit888.com>
2019-02-20 22:09:46 +0800

    refactor

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './pages/Home.vue';
+import Home from './pages/Home/index.vue';
 
 Vue.use(Router);
 
@@ -14,12 +14,17 @@ export default new Router({
       component: Home,
     },
     {
+      path: '/home',
+      name: 'home',
+      component: Home,
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './pages/About.vue'),
+      component: () => import(/* webpackChunkName: "about" */ './pages/About/index.vue'),
     },
   ],
 });
