b91e668f3b6cfeaec4ac1c1a84dc1fe6c61e6dfe
Your Name <you@example.com>
2019-02-20 18:26:34 +0800

    UI

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="home">
     <Header/>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+
     <Footer/>
   </div>
 
