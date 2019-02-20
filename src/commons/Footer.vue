d090901d88d558e96fcb0328dfaee33f124e47f3
LANDAN <landanhsu@bpit888.com>
2019-02-20 16:30:06 +0800

    header, footer

---
+++ src/commons/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="hello">
+    FOOTER
+  </div>
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="home">
+    <Header/>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Footer/>
   </div>
+
+
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Footer from '@/commons/Footer.vue'; // @ is an alias to /src
+import Header from '@/commons/Header.vue'; // @ is an alias to /src
 
 @Component({
   components: {
     HelloWorld,
+    Header,
+    Footer
   },
 })
 export default class Home extends Vue {}
