003e17ee62cde10a75af154e851510f942f8546b
LANDAN <landanhsu@bpit888.com>
2019-02-22 03:14:43 +0800

    change directory

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './pages/Home/index.vue';
+import Home from '@/pages/Home/index.vue';
 
 Vue.use(Router);
 
@@ -24,7 +24,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './pages/About/index.vue'),
+      component: () => import(/* webpackChunkName: "about" */ '@/pages/About/index.vue'),
     },
   ],
 });
