7e811c4aad821aee8b7cc807790b1f224eeed865
LANDAN <landanhsu@bpit888.com>
2019-02-22 06:28:34 +0800

    Modified

--- src/commons/Nav/index.vue
+++ src/commons/Nav/index.vue
@@ -1,35 +1,77 @@
 <template>
- 	<header>
-		<div class="top_nav">
-			<div class="nav_top2">
-				<ul class="nav_ul">
-					<li>首页</li>
-					<li>热门彩种</li>
-					<li>时时彩</li>
-					<li>PK10</li>
-					<li>11选5</li>
-					<li>快三</li>
-					<li>其他</li>
-				</ul>
-			</div>
-		</div>
-		
-	</header>
+  <header>
+    <div class="top_nav">
+      <div class="nav_top2">
+        <ul class="nav_ul">
+          <li>首页</li>
+          <li>热门彩种</li>
+          <li>时时彩</li>
+          <li>PK10</li>
+          <li>11选5</li>
+          <li>快三</li>
+          <li>其他</li>
+        </ul>
+      </div>
+    </div>
+  </header>
 </template>
 <style scoped lang="scss">
-.top_nav{height: 50px;background-color: #37b0e9;}
-		.nav_top2 ul li{text-align: center;height: 50px;width: 170px;float: left;font-size: 16px;color: white;line-height: 50px;}
-	.top{background-color: #ebebeb;height: 30px;}
-		.nav_top,.nav_top1,.nav_top2{width: 1200px; margin: 0 auto;}
-		.top .nav_top{font-size: 14px;line-height: 30px;}
-		.top .nav_top img{margin-right: 5px; margin-top: -3px;}
-			.FloatRight{float: right;}
-	.top_logo{height: 100px;}
-	.top_nav{height: 50px;background-color: #37b0e9;}
-		.nav_top2 ul li{text-align: center;height: 50px;width: 170px;float: left;font-size: 16px;color: white;line-height: 50px;}
-		.displayN{display: none !important;}
-		.displayB{display: block !important;}
-	
+.top_nav {
+  height: 50px;
+  background-color: #37b0e9;
+}
+.nav_top2 ul li {
+  text-align: center;
+  height: 50px;
+  width: 170px;
+  float: left;
+  font-size: 16px;
+  color: white;
+  line-height: 50px;
+}
+.top {
+  background-color: #ebebeb;
+  height: 30px;
+}
+.nav_top,
+.nav_top1,
+.nav_top2 {
+  width: 1200px;
+  margin: 0 auto;
+}
+.top .nav_top {
+  font-size: 14px;
+  line-height: 30px;
+}
+.top .nav_top img {
+  margin-right: 5px;
+  margin-top: -3px;
+}
+.FloatRight {
+  float: right;
+}
+.top_logo {
+  height: 100px;
+}
+.top_nav {
+  height: 50px;
+  background-color: #37b0e9;
+}
+.nav_top2 ul li {
+  text-align: center;
+  height: 50px;
+  width: 170px;
+  float: left;
+  font-size: 16px;
+  color: white;
+  line-height: 50px;
+}
+.displayN {
+  display: none !important;
+}
+.displayB {
+  display: block !important;
+}
 </style>
 
 <script lang="ts">
@@ -42,4 +84,3 @@ class Nav extends Vue {
 
 export default Nav;
 </script>
-
