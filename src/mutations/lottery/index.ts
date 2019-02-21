a1b103678d1cfcaf1fec72e1ede8a658e85c21d1
LANDAN <landanhsu@bpit888.com>
2019-02-21 16:40:28 +0800

    Modified, vuex

--- src/mutations/lottery/index.ts
+++ src/mutations/lottery/index.ts
@@ -2,8 +2,17 @@
 // 将 数据转换 成 vuex 仓库 （store） 格式的机制
 
 let lottery = {
-  LOTTERY_SHOW_MUTATION(state: any, aLotteries: any[]) {
-    state.lotteries = aLotteries;
+  LOTTERY_SHOW_MUTATION(state: any, mLotteries: any[]) {
+    debugger;
+    let oLotteries: any = (mLotteries instanceof Array) ? {} : mLotteries;
+    if (mLotteries instanceof Array) {
+      oLotteries = mLotteries.reduce((_oLotteries, aLottery, {}) => {
+        _oLotteries[aLottery.id] = aLottery;
+        return _oLotteries;
+      });
+      mLotteries = oLotteries;
+    }
+    state.lotteries = oLotteries;
   },
 };
 
