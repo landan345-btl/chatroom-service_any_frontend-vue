dd60183b8b10017d6503054c2c4df9a9b524a582
kangrujie <523829626@qq.com>
2019-02-22 11:30:09 +0800

    html

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -6,37 +6,37 @@
     </Header>
     <main class="home">
       <div v-for="aLottery in oLotteries" :key="aLottery.id">
-        ID: {{aLottery.id}}, name: {{aLottery.name}}
-      </div>
-      <div class="top_content">
-        <div class="content">
-          <div class="displayB">
-            <table border="0">
-              <tr><th>当期奖号</th><th>下期倒计时</th><th>走势图</th></tr>
-              <tr><td>Data</td><td>Data</td><td><img src="@/assets/images/icon03.png"/></td></tr>
-              <tr><td>Data</td><td>Data</td><td><img src="@/assets/images/icon03.png"/></td></tr>
-            </table>
-          </div>
-          <div>
-            2
-          </div>
-          <div>
-            3
-          </div>
-          <div>
-            4
-          </div>
-          <div>
-            5
-          </div>
-          <div>
-            6
-          </div>
-          <div>
-            7
+    
+        <div class="top_content">
+          <div class="content">
+            <div class="displayB">
+              <table border="0" v-on:click="ff">
+                <tr><th>当期奖号</th><th>下期倒计时</th><th>走势图</th></tr>
+                <tr><td> ID: {{aLottery.id}}</td><td> name: {{aLottery.name}}</td><td><img src="@/assets/images/icon03.png"/></td></tr>
+              </table>
+            </div>
+            <div>
+              2
+            </div>
+            <div>
+              3
+            </div>
+            <div>
+              4
+            </div>
+            <div>
+              5
+            </div>
+            <div>
+              6
+            </div>
+            <div>
+              7
+            </div>
           </div>
         </div>
       </div>
+      
     </main>
     <Footer>
     </Footer>
@@ -122,6 +122,10 @@ class Home extends Vue {
   public get oLotteries(): object {
     debugger;
     return this.$store.state.lotteries;
+  }
+
+  public ff(): void {
+    console.log('ff');
   }
 }
 
