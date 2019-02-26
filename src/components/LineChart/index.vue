6a010435aa58fb73012bbaf4e90fb0dde533b6a8
landanhsu <landanhsu@bpit888.com>
2019-02-26 20:01:06 +0000

    Edited

--- src/components/LineChart/index.vue
+++ src/components/LineChart/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <ve-line :data="chartData"></ve-line>
+  <ve-line :data="oData"></ve-line>
 </template>
 
 <style scoped lang="scss">
@@ -13,17 +13,7 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 })
 class LineChart extends Vue {
   @Prop({ default: '' })
-  public chartData: any = {
-    columns: ['日期', '双数', '单数'],
-    rows: [
-      { 日期: '2019 1/1', 双数: 2, 单数: 8 },
-      { 日期: '2019 1/2', 双数: 5, 单数: 5 },
-      { 日期: '2019 1/3', 双数: 6, 单数: 4 },
-      { 日期: '2019 1/4', 双数: 7, 单数: 3 },
-      { 日期: '2019 1/5', 双数: 1, 单数: 9 },
-      { 日期: '2019 1/6', 双数: 4, 单数: 6 },
-    ],
-  };
+  public oData: any = null;
 }
 
 export default LineChart;
