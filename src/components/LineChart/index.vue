404373f3e0d552a9b123e4109418dd4f07911d78
landanhsu <landanhsu@bpit888.com>
2019-02-26 19:59:14 +0000

    Edited

--- src/components/LineChart/index.vue
+++ src/components/LineChart/index.vue
@@ -14,14 +14,14 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 class LineChart extends Vue {
   @Prop({ default: '' })
   public chartData: any = {
-    columns: ['Date', 'User', 'Order'],
+    columns: ['日期', '双数', '单数'],
     rows: [
-      { Date: '1/1', User: 1, Order: 1 },
-      { Date: '1/2', User: 100, Order: 20 },
-      { Date: '1/3', User: 2, Order: 30 },
-      { Date: '1/4', User: 55, Order: 50 },
-      { Date: '1/5', User: 12, Order: 70 },
-      { Date: '1/6', User: 73, Order: 90 },
+      { 日期: '2019 1/1', 双数: 2, 单数: 8 },
+      { 日期: '2019 1/2', 双数: 5, 单数: 5 },
+      { 日期: '2019 1/3', 双数: 6, 单数: 4 },
+      { 日期: '2019 1/4', 双数: 7, 单数: 3 },
+      { 日期: '2019 1/5', 双数: 1, 单数: 9 },
+      { 日期: '2019 1/6', 双数: 4, 单数: 6 },
     ],
   };
 }
