b3e64f99b5412022af95502e1613defd111606d1
LD <C>
2019-03-18 17:09:14 +0800

    rid of css

--- src/commons/Header/index.vue
+++ src/commons/Header/index.vue
@@ -1,24 +1,17 @@
 <template>
   <header>
-    <div class="top">
-      <el-row :gutter="0" type="flex" justify="space-between">
-        <el-col :xs="{span: 11, offset: 1}" :sm="{span: 10, offset: 2}" :md="{span: 9, offset: 3}" :lg="{span: 8, offset: 4}">
-          <span class="notification">
-            <img src="@/assets/images/icon-notification.png"/>
-            <span>最新公告：
-            </span>
-          </span>
-        </el-col>
-        <el-col :xs="{span: 11}" :sm="{span: 10}" :md="{span: 9}" :lg="{span: 8}">
-          <span class="tool">
-            <span>设为首页</span>
-            <span>收藏本站</span>
-            <span>帮助中心</span>
-            <span>手机版</span>
-          </span>
-        </el-col>
-      </el-row>
-    </div>
+    <el-row :gutter="0" type="flex" justify="space-between" class ="top">
+      <el-col :xs="{span: 11, offset: 1}" :sm="{span: 10, offset: 2}" :md="{span: 9, offset: 3}" :lg="{span: 8, offset: 4}" class="notification">
+        <img src="@/assets/images/icon-notification.png"/>
+        <span>最新公告：</span>
+      </el-col>
+      <el-col :xs="{span: 11}" :sm="{span: 10}" :md="{span: 9}" :lg="{span: 8}" class="tool">
+        <span>设为首页</span>
+        <span>收藏本站</span>
+        <span>帮助中心</span>
+        <span>手机版</span>
+      </el-col>
+    </el-row>
     <div class="images">
       <el-row :gutter="0">
         <el-col :xs="{span: 22, offset: 1}" :sm="{span: 20, offset: 2}" :md="{span: 18, offset: 3}" :lg="{span: 16, offset: 4}">
