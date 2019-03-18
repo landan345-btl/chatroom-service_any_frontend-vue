0440fe63673d64a74eff23fff34f2a65702424ed
LD <C>
2019-03-18 17:06:05 +0800

    css

--- src/commons/Header/index.vue
+++ src/commons/Header/index.vue
@@ -1,13 +1,15 @@
 <template>
   <header>
     <div class="top">
-      <el-row :gutter="0">
-        <el-col :xs="{span: 22, offset: 1}" :sm="{span: 20, offset: 2}" :md="{span: 18, offset: 3}" :lg="{span: 16, offset: 4}">
+      <el-row :gutter="0" type="flex" justify="space-between">
+        <el-col :xs="{span: 11, offset: 1}" :sm="{span: 10, offset: 2}" :md="{span: 9, offset: 3}" :lg="{span: 8, offset: 4}">
           <span class="notification">
             <img src="@/assets/images/icon-notification.png"/>
             <span>最新公告：
             </span>
           </span>
+        </el-col>
+        <el-col :xs="{span: 11}" :sm="{span: 10}" :md="{span: 9}" :lg="{span: 8}">
           <span class="tool">
             <span>设为首页</span>
             <span>收藏本站</span>
@@ -17,14 +19,14 @@
         </el-col>
       </el-row>
     </div>
-
-		<div class="top_logo">
-			<div class="nav_top1">
-				<a href="/"><img src="@/assets/images/logo.png"/></a>
-				<img class="img" src="@/assets/images/icon02.png"/>
-			</div>
-		</div>
-
+    <div class="images">
+      <el-row :gutter="0">
+        <el-col :xs="{span: 22, offset: 1}" :sm="{span: 20, offset: 2}" :md="{span: 18, offset: 3}" :lg="{span: 16, offset: 4}">
+          <a href="/"><img src="@/assets/images/logo.png"/></a>
+				  <img class="img" src="@/assets/images/icon-qrcode.png"/>
+        </el-col>
+      </el-row>
+    </div>
   </header>
 </template>
 <style scoped lang="scss">
