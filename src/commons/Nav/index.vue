ab41c33cb13a708533f24a005801d782ffef9e78
landan345 <landan345.btl@gmail.com>
2019-03-19 15:56:05 +0800

    rid of useless code

--- src/commons/Nav/index.vue
+++ src/commons/Nav/index.vue
@@ -1,24 +1,20 @@
 <template>
  	<nav>
-		<div class="top_nav">
-			<div class="nav_top2">
-				<ul class="nav_ul">
-					<li v-on:click="handleHome">首页</li>
-					<li v-on:click="handlePapularLottery">彩票大厅</li>
-					<li v-on:click="handleInstantLottery">走势图表</li>
-					<li v-on:click="handlePKTenLottery">追号计划</li>
-				</ul>
-			</div>
+		<div class="menu">
+      <ul>
+        <li v-on:click="handleHome">首页</li>
+        <li v-on:click="handlePapularLottery">彩票大厅</li>
+        <li v-on:click="handleInstantLottery">走势图表</li>
+        <li v-on:click="handlePKTenLottery">追号计划</li>
+      </ul>
 		</div>
-		
 	</nav>
 </template>
 <style scoped lang="scss">
-.top_nav{
+.menu {
   height: 50px;
   background-color: #ff7b00;
-}
-	.nav_top2 ul li{
+  ul li{
     text-align: center;
     height: 50px;
     width: 90px;
@@ -28,18 +24,8 @@
     line-height: 50px;
     cursor: pointer;
   }
-	.top{background-color: #ebebeb;height: 30px;}
-		.nav_top,.nav_top1,.nav_top2{width: 1200px; margin: 0 auto;}
-		.top .nav_top{font-size: 14px;line-height: 30px;}
-		.top .nav_top img{margin-right: 5px; margin-top: -3px;}
-			.FloatRight{float: right;}
-	.top_logo{height: 100px;}
+}
 
-	 @media all and (max-width: 820px) {
-        .top_nav {height: 100px; width:100% } 
-        .nav_top2 ul li{text-align: center; height: 100px; width: 14%; float: left; font-size: 41px; color: white; line-height: 100px;}
-       
-        }
 </style>
 
 <script lang="ts">
