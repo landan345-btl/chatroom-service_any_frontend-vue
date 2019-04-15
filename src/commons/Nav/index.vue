1feaa646dc410dfffa79efd2ed3a716ed56d1e77
landan345 <landan345.btl@gmail.com>
2019-04-16 02:21:43 +0800

    p

--- src/commons/Nav/index.vue
+++ src/commons/Nav/index.vue
@@ -3,32 +3,46 @@
     <el-row :gutter="0">
       <el-col :xs="{span: 22, offset: 1}" :sm="{span: 20, offset: 2}" :md="{span: 18, offset: 3}" :lg="{span: 16, offset: 4}">
         <el-menu :default-active="activeIndex2" class="el-menu-demo nav-background" mode="horizontal" @select="handleSelect" text-color="#fff" active-text-color="#ffd04b">
-          <el-menu-item index="1"></el-menu-item>
+          <el-menu-item index="1" class="font-vmin">首页</el-menu-item>
           <el-submenu index="2">
-            <template slot="title">我的工作台</template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-            <el-menu-item index="2-3">选项3</el-menu-item>
+            <template slot="title">彩票大厅</template>
+            <el-menu-item index="2-1">热门彩</el-menu-item>
+            <el-menu-item index="2-2">极速彩</el-menu-item>
+            <el-menu-item index="2-3">高频彩</el-menu-item>
+            <el-menu-item index="2-3">境外彩</el-menu-item>
+            <el-menu-item index="2-3">全国彩</el-menu-item>
             <el-submenu index="2-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="2-4-1">选项1</el-menu-item>
-              <el-menu-item index="2-4-2">选项2</el-menu-item>
-              <el-menu-item index="2-4-3">选项3</el-menu-item>
             </el-submenu>
           </el-submenu>
-          <el-menu-item index="3" disabled>消息中心</el-menu-item>
-          <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+          <el-menu-item index="3">长龙提醒</el-menu-item>
+          <el-menu-item index="4"><a href="/trend" target="_blank">走势图表</a></el-menu-item>
         </el-menu>
       </el-col>
     </el-row>
 	</nav>
 </template>
 <style scoped lang="scss">
+  nav {
+    height: 51px;
+  }
   .nav-background {
     background-color: #ff7b00;
   }
-  .namespace .el-menu--horizontal > .el-submenu .el-submenu__title:hover {
-    background: #eb7100;
+  .namespace {
+    .el-menu--horizontal>.el-menu-item:not(.is-disabled):hover {
+      background: #eb7100;
+    }
+    .el-menu--horizontal >.el-menu-item {
+      height: 51px;
+    }
+
+  }
+
+  .namespace .el-menu--horizontal>.el-submenu .el-submenu__title {
+      height: 51px;
+    }
+  .font-vmin {
+    font-size: 2vmin;
   }
 </style>
 
