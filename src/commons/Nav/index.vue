ad901c4ad4a07d052ad52f5b861cea80241f0638
landan345 <landan345.btl@gmail.com>
2019-04-15 23:06:04 +0800

    p

--- src/commons/Nav/index.vue
+++ src/commons/Nav/index.vue
@@ -1,41 +1,35 @@
 <template>
- 	<nav>
-    <el-row :gutter="0" class="menu">
+ 	<nav class="nav-background namespace">
+    <el-row :gutter="0">
       <el-col :xs="{span: 22, offset: 1}" :sm="{span: 20, offset: 2}" :md="{span: 18, offset: 3}" :lg="{span: 16, offset: 4}">
-        <ul>
-          <li v-on:click="handleHome">首页</li>
-          <li v-on:click="handlePapularLottery">彩票大厅</li>
-          <li v-on:click="handleInstantLottery">走势图表</li>
-          <li v-on:click="handlePKTenLottery">追号计划</li>
-        </ul>
+        <el-menu :default-active="activeIndex2" class="el-menu-demo nav-background" mode="horizontal" @select="handleSelect" text-color="#fff" active-text-color="#ffd04b">
+          <el-menu-item index="1"></el-menu-item>
+          <el-submenu index="2">
+            <template slot="title">我的工作台</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+            <el-menu-item index="2-3">选项3</el-menu-item>
+            <el-submenu index="2-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="2-4-1">选项1</el-menu-item>
+              <el-menu-item index="2-4-2">选项2</el-menu-item>
+              <el-menu-item index="2-4-3">选项3</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="3" disabled>消息中心</el-menu-item>
+          <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+        </el-menu>
       </el-col>
     </el-row>
 	</nav>
 </template>
 <style scoped lang="scss">
-.menu {
-  height: 50px;
-  background: #ff7b00;
-  ul {
-    li:first-child {
-      width: 5vmin;
-    }
-    li {
-      text-align: center;
-      height: 50px;
-      width: 11vmin;
-      float: left;
-      font-size: 16px;
-      color: white;
-      line-height: 50px;
-      cursor: pointer;
-    }
-    li:hover {
-      background: #eb7100;
-    }
+  .nav-background {
+    background-color: #ff7b00;
   }
-}
-
+  .namespace .el-menu--horizontal > .el-submenu .el-submenu__title:hover {
+    background: #eb7100;
+  }
 </style>
 
 <script lang="ts">
