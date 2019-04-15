7b4fa3a51b09022e4d84c12da0acf22f28a106ff
landan345 <landan345.btl@gmail.com>
2019-04-16 01:48:03 +0800

    env

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,15 @@
 module.exports = {
     configureWebpack: {
       devServer: {
-          watchOptions: {
-            ignored: ['node_modules'],
-            aggregateTimeout: 300,
-            poll: 1500
-          },
-          public: '127.0.0.1', // vagrant machine address,
-          disableHostCheck: true,
+        host: process.env.HOST,
+        port: process.env.PORT,
+        watchOptions: {
+          ignored: ['node_modules'],
+          aggregateTimeout: 300,
+          poll: 1500
+        },
+        public: '127.0.0.1', // vagrant machine address,
+        disableHostCheck: true,
       }
     }
   }
