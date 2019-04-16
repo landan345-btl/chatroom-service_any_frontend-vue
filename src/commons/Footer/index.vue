f1b372033ea07f865125f800888258e4b5ad2223
landan345 <landan345.btl@gmail.com>
2019-04-17 02:17:17 +0800

    css

--- src/commons/Footer/index.vue
+++ src/commons/Footer/index.vue
@@ -13,21 +13,21 @@
    			<li>代理加盟</li> 	 			
   		</ul><br>
   	</div>
-		<div class="bottom">
-  		<p>© 2019 www.168kai.com All rights reserved 彩票开奖网 版权所有 粤ICP备09298233号站长统计</p>
-		</div>
+    <div class="bottom">
+      <el-row :gutter="0" type="flex" justify="space-between" class="top">
+        <el-col :xs="{span: 22, offset: 1}" :sm="{span: 20, offset: 2}" :md="{span: 18, offset: 3}" :lg="{span: 16, offset: 4}">
+          <p>© 2019 www.168kai.com All rights reserved 彩票开奖网 版权所有 粤ICP备09298233号站长统计</p>
+        </el-col>
+      </el-row>
+    </div>
+
   </footer>
 </template>
+
 <style scoped lang="scss">
-	footer {
-		position: absolute;
-		width: 100%;
-		bottom: 0;
-		background-color: #3f404f;
-		text-align: center;
-		color:#999999;
-	}
+@import 'Index-scoped.scss';
 </style>
+
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 @Component({
