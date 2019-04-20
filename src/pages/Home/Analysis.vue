b857f8daf9d9d5069c6418ac0bdd177a59bc3086
landan345 <landan345.btl@gmail.com>
2019-04-21 00:01:10 +0800

    p

--- src/pages/Home/Analysis.vue
+++ src/pages/Home/Analysis.vue
@@ -4,67 +4,67 @@
       <span class="title font-weight-bold">分析工具</span>
       <span class="more position-absolute">更多</span>
     </div>
-    <div class="line">
-    </div>
     <div class="lotteries">
-      <div class="lottery" v-if="iIndex <= 1" v-for="(oLottery, sCode, iIndex) in lotteries" :key="sCode">
-        <div class="top">
-          <span class="icon d-inline-block align-middle">
-            <img :src="require(`@/assets/images/` + `${sCode}` + `-md.png`)"  />
-          </span>
-          <span class="title align-middle">
-            {{oLottery.name}}
-          </span>
-        </div>
-        <div class="line">
-        </div>
-        <div class="trend">
-          <div class="top">          
-            <font-awesome-icon icon="chart-line" class="d-inline-block color-deepskyblue font-size-2p5"/>
-            <span>&nbsp;走势</span>
-          </div>
-          <ul class="middle list">
-            <router-link class="" :to="{ path: '/trend', query: { code: `${sCode}`, type: 1 }}">
-              <li>冠亚和走势</li>
-            </router-link>
-            <router-link class="" :to="{ path: '/trend', query: { code: `${sCode}`, type: 2 }}">
-              <li>位置走势</li>
-            </router-link>
-            <router-link class="" :to="{ path: '/trend', query: { code: `${sCode}`, type: 3 }}">
-              <li>号码走势</li>
-            </router-link>
-          </ul>
-        </div>
-        <div class="line">
-        </div>
-        <div class="candlestick">
+      <div  v-if="(iIndex - Math.floor((Math.random() * 8) + 1)) >= -1 && (iIndex - Math.floor((Math.random() * 8) + 1)) <= 1" v-for="(oLottery, sCode, iIndex) in lotteries" :key="sCode">
+        <div class="line"></div>
+        <div class="lottery">
           <div class="top">
-            <font-awesome-icon icon="search-dollar" class="d-inline-block color-red font-size-2p5"/>
-            <span>&nbsp;遗漏</span>
+            <span class="icon d-inline-block align-middle">
+              <img :src="require(`@/assets/images/` + `${sCode}` + `-md.png`)"  />
+            </span>
+            <span class="title align-middle">
+              {{oLottery.name}}
+            </span>
           </div>
-          <ul class="middle list">
-            <router-link class="" :to="{ path: '/trend', query: { code: `${sCode}`, type: 4 }}">
-              <li>综合分析</li>
-            </router-link>
-            <router-link class="" :to="{ path: '/trend', query: { code: `${sCode}`, type: 5 }}">
-              <li>定位走势</li>
-            </router-link>
-            <router-link class="" :to="{ path: '/trend', query: { code: `${sCode}`, type: 6 }}">
-              <li>第二球走势</li>
-            </router-link>
-            <router-link class="" :to="{ path: '/trend', query: { code: `${sCode}`, type: 7 }}">
-              <li>第三球走势</li>
-            </router-link>
-            <router-link class="" :to="{ path: '/trend', query: { code: `${sCode}`, type: 8 }}">
-              <li>第四球走势</li>
-            </router-link>
-              <router-link class="" :to="{ path: '/trend', query: { code: `${sCode}`, type: 9 }}">
-              <li>综合分析</li>
-            </router-link>
-          </ul>
+          <div class="line">
+          </div>
+          <div class="trend">
+            <div class="top">          
+              <font-awesome-icon icon="chart-line" class="d-inline-block color-deepskyblue font-size-2p5"/>
+              <span>&nbsp;走势</span>
+            </div>
+            <ul class="middle list">
+              <router-link class="" :to="{ path: '/trend', query: { code: `${sCode}`, type: 1 }}">
+                <li>冠亚和走势</li>
+              </router-link>
+              <router-link class="" :to="{ path: '/trend', query: { code: `${sCode}`, type: 2 }}">
+                <li>位置走势</li>
+              </router-link>
+              <router-link class="" :to="{ path: '/trend', query: { code: `${sCode}`, type: 3 }}">
+                <li>号码走势</li>
+              </router-link>
+            </ul>
+          </div>
+          <div class="line">
+          </div>
+          <div class="candlestick">
+            <div class="top">
+              <font-awesome-icon icon="search-dollar" class="d-inline-block color-red font-size-2p5"/>
+              <span>&nbsp;遗漏</span>
+            </div>
+            <ul class="middle list">
+              <router-link class="" :to="{ path: '/trend', query: { code: `${sCode}`, type: 4 }}">
+                <li>综合分析</li>
+              </router-link>
+              <router-link class="" :to="{ path: '/trend', query: { code: `${sCode}`, type: 5 }}">
+                <li>定位走势</li>
+              </router-link>
+              <router-link class="" :to="{ path: '/trend', query: { code: `${sCode}`, type: 6 }}">
+                <li>第二球走势</li>
+              </router-link>
+              <router-link class="" :to="{ path: '/trend', query: { code: `${sCode}`, type: 7 }}">
+                <li>第三球走势</li>
+              </router-link>
+              <router-link class="" :to="{ path: '/trend', query: { code: `${sCode}`, type: 8 }}">
+                <li>第四球走势</li>
+              </router-link>
+                <router-link class="" :to="{ path: '/trend', query: { code: `${sCode}`, type: 9 }}">
+                <li>综合分析</li>
+              </router-link>
+            </ul>
+          </div>
         </div>
       </div>
-      <div class="line"></div>
     </div>
   </div>
 </template>
