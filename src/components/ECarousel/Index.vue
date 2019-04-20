463bef2d0e5af513bbd39f7380ebc392902af1d2
landan345 <landan345.btl@gmail.com>
2019-04-21 02:14:04 +0800

    p

--- src/components/ECarousel/Index.vue
+++ src/components/ECarousel/Index.vue
@@ -2,8 +2,8 @@
   <div class="block">
     <span class="demonstration">默认 Hover 指示器触发</span>
     <el-carousel height="150px">
-      <el-carousel-item v-for="item in 4" :key="item">
-        <h3>{{ item }}</h3>
+      <el-carousel-item v-for="iNumber in 3" :key="iNumber" >
+        <img :src="require(`@/assets/images/banner-` + `1` + `.jpg`)" />
       </el-carousel-item>
     </el-carousel>
   </div>
