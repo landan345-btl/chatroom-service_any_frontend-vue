1bc26ac5643892ea43e90d5a8a82aaba20e80424
landan345 <landan345.btl@gmail.com>
2019-04-22 15:09:20 +0800

    p

--- src/Components/ECarousel/Index.vue
+++ src/Components/ECarousel/Index.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="block">
+
     <el-carousel height="300px">
-      <el-carousel-item v-for="iNumber in 3" :key="iNumber" >
-        <img :src="require(`@/assets/images/banner-` + `1` + `.jpg`)" />
+      <el-carousel-item v-for="(sImage, iIndex) in images" :key="iIndex" >
+        <img :src="require(`${sImage}`)" />
+              {{sImage}}
       </el-carousel-item>
     </el-carousel>
   </div>
@@ -19,23 +21,8 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
   name: 'ECarousel',
 })
 class ECarousel extends Vue {
-  @Prop({ default: null })
-  public options: any = [{
-    value: '选项1',
-    label: '黄金糕',
-  }, {
-    value: '选项2',
-    label: '双皮奶',
-  }, {
-    value: '选项3',
-    label: '蚵仔煎',
-  }, {
-    value: '选项4',
-    label: '龙须面',
-  }, {
-    value: '选项5',
-    label: '北京烤鸭',
-  }];
+  @Prop()
+  public images: any;
 }
 export default ECarousel;
 
--- src/Pages/Home/News.vue
+++ src/Pages/Home/News.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="news">
     <span class="left d-inline-block">
-      <E-carousel />
+      <E-carousel v-bind:images="images"/>
     </span>
     <span class="right d-inline-block">
       <div class="top position-relative">
         <span class="title font-weight-bold">行业新闻</span>
         <span class="more position-absolute">更多</span>
+      </div>
+      <div class="messages" v-for="(sMessage, iIndex) in messages" :key="iIndex">
+        <div class="message">
+          {{sMessage}}
+        </div>
       </div>
     </span>
   </div>
@@ -31,7 +36,20 @@ let oAxiosHelper = new AxiosHelper();
   },
 })
 class Lottery extends Vue {
+  public messages: string[] = [
+    '山西11选5号码推荐4月19日上午',
+    '辽宁11选5号码推荐4月19日上午',
+    '吉林11选5号码推荐4月19日上午',
+    '安徽快三出号分析对子11转热 4月18日',
+    '吉林快三出号分析对子11转热 4月18日',
+    '江西快三出号分析对子22转热 4月18日',
+  ];
 
+  public images: string[] = [
+    '@/assets/images/banner-1.jpg',
+    '@/assets/images/banner-2.jpg',
+    '@/assets/images/banner-3.jpg',
+  ];
 }
 
 export default Lottery;
