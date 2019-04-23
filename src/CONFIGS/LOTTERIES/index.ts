20048a34604ffb7e8937d3595e0e4b6a733f4272
landan345 <landan345.btl@gmail.com>
2019-04-23 23:28:32 +0800

    p

--- src/CONFIGS/LOTTERIES/index.ts
+++ src/CONFIGS/LOTTERIES/index.ts
@@ -15,6 +15,10 @@ const LOTTERIES: I_LOTTERIES = {
     NAME: '极速赛车',
     SRC: '@/assets/images/JSPK10-md.png',
   },
+  FFPK10: {
+    NAME: '分分PK10',
+    SRC: '@/assets/images/FFPK10-md.png',
+  },
   PCDD: {
     NAME: 'PC蛋蛋28',
     SRC: '@/assets/images/PCDD-md.png',
@@ -27,25 +31,173 @@ const LOTTERIES: I_LOTTERIES = {
     NAME: '广东快乐十分',
     SRC: '@/assets/images/GDKLSF-md.png',
   },
-  // JX11X5: {
-  //   NAME: '江西11选5',
-  //   SRC: '@/assets/images/JX11X5-md.png',
-  // },
-  SD11X5: {
-    NAME: '十一运夺金',
-    SRC: '@/assets/images/SD11X5-md.png',
+  PAOMA: {
+    NAME: '香港跑马',
+    SRC: '@/assets/images/PAOMA-md.png',
+  },
+  '3FPK10': {
+    NAME: '三分PK10',
+    SRC: '@/assets/images/3FPK10-md.png',
+  },
+  CQSSC: {
+    NAME: '重庆时时彩',
+    SRC: '@/assets/images/CQSSC-md.png',
+  },
+  XJSSC: {
+    NAME: '新疆时时彩',
+    SRC: '@/assets/images/XJSSC-md.png',
+  },
+  HLJSSC: {
+    NAME: '黑龙江时时彩',
+    SRC: '@/assets/images/HLJSSC-md.png',
+  },
+  AFGH90: {
+    NAME: '加纳1.5分彩',
+    SRC: '@/assets/images/AFGH90-md.png',
+  },
+  TW5FC: {
+    NAME: '台湾5分彩',
+    SRC: '@/assets/images/TW5FC-md.png',
+  },
+  FFSSC: {
+    NAME: '幸运分分彩',
+    SRC: '@/assets/images/FFSSC-md.png',
+  },
+  BJ5FC: {
+    NAME: '北京5分彩',
+    SRC: '@/assets/images/BJ5FC-md.png',
+  },
+  '3FSSC': {
+    NAME: '幸运三分彩',
+    SRC: '@/assets/images/3FSSC-md.png',
   },
   JSK3: {
     NAME: '江苏快3',
     SRC: '@/assets/images/JSK3-md.png',
   },
+  JLK3: {
+    NAME: '吉林快3',
+    SRC: '@/assets/images/JLK3-md.png',
+  },
+  AHK3: {
+    NAME: '安徽快3',
+    SRC: '@/assets/images/AHK3-md.png',
+  },
+  HUBK3: {
+    NAME: '湖北快3',
+    SRC: '@/assets/images/HUBK3-md.png',
+  },
+  HEBK3: {
+    NAME: '河北快3',
+    SRC: '@/assets/images/HEBK3-md.png',
+  },
+  GXK3: {
+    NAME: '广西快3',
+    SRC: '@/assets/images/GXK3-md.png',
+  },
+  FFK3: {
+    NAME: '幸运快3',
+    SRC: '@/assets/images/FFK3-md.png',
+  },
+  TJKLSF: {
+    NAME: '天津快乐十分',
+    SRC: '@/assets/images/TJKLSF-md.png',
+  },
+  HUNKLSF: {
+    NAME: '湖南快乐十分',
+    SRC: '@/assets/images/HUNKLSF-md.png',
+  },
+  GD11X5: {
+    NAME: '广东11选5',
+    SRC: '@/assets/images/GD11X5-md.png',
+  },
+  SD11X5: {
+    NAME: '山东11选5',
+    SRC: '@/assets/images/SD11X5-md.png',
+  },
+  JX11X5: {
+    NAME: '江西11选5',
+    SRC: '@/assets/images/JX11X5-md.png',
+  },
+  SH11X5: {
+    NAME: '上海11选5',
+    SRC: '@/assets/images/SH11X5-md.png',
+  },
+  ZJ11X5: {
+    NAME: '浙江11选5',
+    SRC: '@/assets/images/ZJ11X5-md.png',
+  },
+  AH11X5: {
+    NAME: '安徽11选5',
+    SRC: '@/assets/images/AH11X5-md.png',
+  },
   BJ11X5: {
     NAME: '北京11选5',
     SRC: '@/assets/images/BJ11X5-md.png',
   },
+  FJ11X5: {
+    NAME: '福建11选5',
+    SRC: '@/assets/images/FJ11X5-md.png',
+  },
+  JS11X5: {
+    NAME: '江苏11选5',
+    SRC: '@/assets/images/JS11X5-md.png',
+  },
+  // JX11X5: {
+  //   NAME: '江西11选5',
+  //   SRC: '@/assets/images/JX11X5-md.png',
+  // },
+  HLJ11X5: {
+    NAME: '黑龙江11选5',
+    SRC: '@/assets/images/HLJ11X5-md.png',
+  },
+  XJ11X5: {
+    NAME: '新疆11选5',
+    SRC: '@/assets/images/XJ11X5-md.png',
+  },
+  FF11X5: {
+    NAME: '幸运11选5',
+    SRC: '@/assets/images/FF11X5-md.png',
+  },
   KRYSU: {
     NAME: '韩国音速',
     SRC: '@/assets/images/KRYSU-md.png',
+  },
+  FC3D: {
+    NAME: '福彩3D',
+    SRC: '@/assets/images/FC3D-md.png',
+  },
+  PL3: {
+    NAME: '排列三',
+    SRC: '@/assets/images/PL3-md.png',
+  },
+  SHSSL: {
+    NAME: '上海时时乐',
+    SRC: '@/assets/images/SHSSL-md.png',
+  },
+  TW3D: {
+    NAME: '台湾3D',
+    SRC: '@/assets/images/TW3D-md.png',
+  },
+  BJKL8: {
+    NAME: '北京快乐8',
+    SRC: '@/assets/images/BJKL8-md.png',
+  },
+  TWBG: {
+    NAME: '台湾宾果',
+    SRC: '@/assets/images/TWBG-md.png',
+  },  
+  HK6: {
+    NAME: '香港六合彩',
+    SRC: '@/assets/images/HK6-md.png',
+  },  
+  '5FSIX': {
+    NAME: '幸运六合彩',
+    SRC: '@/assets/images/5FSIX-md.png',
+  },
+  '5F28': {
+    NAME: '吉利28',
+    SRC: '@/assets/images/5F28-md.png',
   },
 };
 
