10f8eabab2f8819d3fc19859de4717b9873895f7
landan345 <landan345.btl@gmail.com>
2019-04-24 15:02:06 +0800

    p

--- src/Pages/Trend/Index.vue
+++ src/Pages/Trend/Index.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="trend">
     <Header/>
-    <h1>trend </h1>
+    <Nav-top/>
+    <Nav-right/>
     <Footer/>
   </div>
 </template>
@@ -10,10 +11,14 @@ import { Component, Vue } from 'vue-property-decorator';
 
 import Footer from '@/Commons/Footer/Index.vue'; // @ is an alias to /src
 import Header from '@/Commons/Header/Index.vue'; // @ is an alias to /src
+import NavTop from '@/Commons/NavTop/Index.vue';
+import NavRight from '@/Commons/NavRight/Index.vue';
 
 @Component({
   components: {
     Header,
+    NavTop,
+    NavRight,
     Footer,
   },
 })
