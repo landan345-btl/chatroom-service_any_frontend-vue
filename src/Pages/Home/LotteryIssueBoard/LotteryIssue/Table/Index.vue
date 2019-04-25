9fb86ae4d74a7c7b4a220c133cb540d597570b32
landan345 <landan345.btl@gmail.com>
2019-04-25 22:51:38 +0800

    p

---
+++ src/Pages/Home/LotteryIssueBoard/LotteryIssue/Table/Index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="table">
+    <span class="number kl8" v-for="(iNumber, sKey) in numbers" :key="sKey">
+      <span>{{iNumber}}</span>
+    </span>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import 'Index-scoped.scss';
+</style>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'Table',
+})
+class Table extends Vue {
+  @Prop()
+  public code!: string;
+  @Prop()
+  public numbers!: number[];
+  @Prop()
+  public types!: string;
+
+}
+export default Table;
+
+</script>
