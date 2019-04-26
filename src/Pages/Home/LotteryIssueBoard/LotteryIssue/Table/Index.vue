13284719bd41d748448d0deacfa42893ab833030
landan345 <landan345.btl@gmail.com>
2019-04-26 14:58:11 +0800

    p

--- src/Pages/Home/LotteryIssueBoard/LotteryIssue/Table/Index.vue
+++ src/Pages/Home/LotteryIssueBoard/LotteryIssue/Table/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  <table class="text-center">
+  <table class="text-center" v-if="'PK10' === lotteryIssue.types">
     <tr>
       <th colspan=5>1-5龙虎</th>
       <th colspan=3>冠亚和</th>
@@ -14,7 +14,72 @@
       <td>大</td>
       <td>双</td>
     </tr>
-
+  </table>
+  <table class="text-center" v-else-if="'SSC' === lotteryIssue.types">
+    <tr>
+      <th colspan=3>总和</th>
+      <th>龙虎</th>
+      <th>前三</th>
+      <th>中三</th>
+      <th>后三</th>
+    </tr>
+    <tr>
+      <td>27</td>
+      <td>单</td>
+      <td>大</td>
+      <td>龙</td>
+      <td>杂六</td>
+      <td>半顺</td>
+      <td>半顺</td>
+    </tr>
+  </table>
+  <table class="text-center" v-else-if="'11X5' === lotteryIssue.types">
+    <tr>
+      <th colspan=3>总和</th>
+      <th>前三</th>
+      <th>中三</th>
+      <th>后三</th>
+    </tr>
+    <tr>
+      <td>36</td>
+      <td>大</td>
+      <td>双</td>
+      <td>半顺</td>
+      <td>杂六</td>
+      <td>半顺</td>
+    </tr>
+  </table>
+  <table class="text-center" v-else-if="'KLSF' === lotteryIssue.types">
+    <tr>
+      <th colspan=5>1-5龙虎</th>
+      <th colspan=3>冠亚和</th>
+    </tr>
+    <tr>
+      <td>龙</td>
+      <td>虎</td>
+      <td>龙</td>
+      <td>龙</td>
+      <td>龙</td>
+      <td>12</td>
+      <td>大</td>
+      <td>双</td>
+    </tr>
+  </table>
+  <table class="text-center" v-else-if="'K3' === lotteryIssue.types">
+    <tr>
+      <th colspan=5>1-5龙虎</th>
+      <th colspan=3>冠亚和</th>
+    </tr>
+    <tr>
+      <td>龙</td>
+      <td>虎</td>
+      <td>龙</td>
+      <td>龙</td>
+      <td>龙</td>
+      <td>12</td>
+      <td>大</td>
+      <td>双</td>
+    </tr>
   </table> 
 </template>
 
