c8b05a73522e727913c9bec2898ae8646d09c64a
landan345 <landan345.btl@gmail.com>
2019-04-30 23:53:36 +0800

    scc

--- src/Pages/Trend/Index.vue
+++ src/Pages/Trend/Index.vue
@@ -6,22 +6,44 @@
     <el-row :gutter="0">
       <el-col :xs="{span: 22, offset: 1}" :sm="{span: 20, offset: 2}" :md="{span: 18, offset: 3}" :lg="{span: 16, offset: 4}">
         <main class="mt-2">
-          <I-tabs>
-            <TabPane label="PK10" name="PK10">
-            </TabPane>
-            <TabPane label="时时彩" name="SSC">
-            </TabPane>
-            <TabPane label="11选5" name="11X5">
-            </TabPane>
-            <TabPane label="快三" name="K3">
-            </TabPane>
-            <TabPane label="快乐十分" name="KLSF">
-            </TabPane>
-            <TabPane label="快乐8" name="KL8">
-            </TabPane>
-            <TabPane label="六合彩" name="SIX">
-            </TabPane>
-          </I-tabs>
+          <E-tabs>
+            <el-tab-pane label="PK10" name="ss">
+              <E-tabs :type="'card'">
+                <el-tab-pane label="幸运飞艇">
+                  <E-tabs>
+                    <el-tab-pane label="冠军" name="1">
+                    </el-tab-pane>
+                    <el-tab-pane label="亚军" name="2">
+                    </el-tab-pane>
+                  </E-tabs>
+                </el-tab-pane>
+                <el-tab-pane label="北京赛车">
+                  <E-tabs>
+                    <el-tab-pane label="冠军" name="1">
+                    </el-tab-pane>
+                    <el-tab-pane label="亚军" name="2">
+                    </el-tab-pane>
+                  </E-tabs>
+                </el-tab-pane>
+              </E-tabs>
+            </el-tab-pane>
+            <!-- <el-tab-pane label="时时彩" name="SSC">
+              <E-tabs :type="'card'">
+                <el-tab-pane label="重庆时时彩" name="SSC-2">
+                </el-tab-pane>
+              </E-tabs>
+            </el-tab-pane> -->
+            <el-tab-pane label="11选5" name="11X5">
+            </el-tab-pane>
+            <el-tab-pane label="快三" name="K3">
+            </el-tab-pane>
+            <el-tab-pane label="快乐十分" name="KLSF">
+            </el-tab-pane>
+            <el-tab-pane label="快乐8" name="KL8">
+            </el-tab-pane>
+            <el-tab-pane label="六合彩" name="SIX">
+            </el-tab-pane>
+          </E-tabs>
         </main>
       </el-col>
     </el-row>
@@ -39,7 +61,7 @@ import Footer from '@/Commons/Footer/Index.vue'; // @ is an alias to /src
 import Header from '@/Commons/Header/Index.vue'; // @ is an alias to /src
 import NavTop from '@/Commons/NavTop/Index.vue';
 import NavRight from '@/Commons/NavRight/Index.vue';
-import ITabs from '@/Components/ITabs/Index.vue';
+import ETabs from '@/Components/ETabs/Index.vue';
 
 @Component({
   components: {
@@ -47,7 +69,7 @@ import ITabs from '@/Components/ITabs/Index.vue';
     NavTop,
     NavRight,
     Footer,
-    ITabs,
+    ETabs,
   },
 })
 class Trend extends Vue {
