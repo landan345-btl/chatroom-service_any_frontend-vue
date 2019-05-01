27fc471071303ff5f9ce7e42fad1c080876ac763
landan345 <landan345.btl@gmail.com>
2019-05-01 20:12:19 +0800

    /trend

--- src/Pages/Trend/Index.vue
+++ src/Pages/Trend/Index.vue
@@ -5,49 +5,63 @@
     <Nav-right/>
     <el-row :gutter="0">
       <el-col :xs="{span: 22, offset: 1}" :sm="{span: 20, offset: 2}" :md="{span: 18, offset: 3}" :lg="{span: 16, offset: 4}">
-        <main class="mt-2">
-          <E-tabs>
-            <el-tab-pane label="PK10" name="PK10">
-              <E-tabs :type="'card'">
+        <main class="mt-2 mb-2">
+          <E-tabs :type="'card'">
+            <el-tab-pane label="PK拾" name="PK10">
+              <E-tabs>
                 <el-tab-pane :label="oLottery.code" :name="oLottery.code" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'PK10' === oLottery.types">
-                  {{ oLottery.name }}
                 </el-tab-pane>
               </E-tabs>
+              <Lottery :code="oLottery.code" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'PK10' === oLottery.types" class="d-inline-block"/>
+            </el-tab-pane>
+            <el-tab-pane label="时时彩" name="SSC">
+              <E-tabs>
+                <el-tab-pane :label="oLottery.code" :name="oLottery.code" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'SSC' === oLottery.types">
+                </el-tab-pane>
+              </E-tabs>
+              <Lottery :code="oLottery.code" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'SSC' === oLottery.types" class="d-inline-block"/>
             </el-tab-pane>
             <el-tab-pane label="11选5" name="11X5">
-              <E-tabs :type="'card'">
+              <E-tabs>
                 <el-tab-pane :label="oLottery.code" :name="oLottery.code" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'11X5' === oLottery.types">
-                  {{ oLottery.name }}
                 </el-tab-pane>
               </E-tabs>
+              <Lottery :code="oLottery.code" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'11X5' === oLottery.types" class="d-inline-block"/>
             </el-tab-pane>
             <el-tab-pane label="快三" name="K3">
-              <E-tabs :type="'card'">
+              <E-tabs>
                 <el-tab-pane :label="oLottery.code" :name="oLottery.code" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'K3' === oLottery.types">
-                  {{ oLottery.name }}
                 </el-tab-pane>
               </E-tabs>
+              <Lottery :code="oLottery.code" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'K3' === oLottery.types" class="d-inline-block"/>
             </el-tab-pane>
             <el-tab-pane label="快乐十分" name="KLSF">
-              <E-tabs :type="'card'">
+              <E-tabs>
                 <el-tab-pane :label="oLottery.code" :name="oLottery.code" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'KLSF' === oLottery.types">
-                  {{ oLottery.name }}
                 </el-tab-pane>
               </E-tabs>
+              <Lottery :code="oLottery.code" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'KLSF' === oLottery.types" class="d-inline-block"/>
             </el-tab-pane>
             <el-tab-pane label="快乐8" name="KL8">
-              <E-tabs :type="'card'">
+              <E-tabs>
                 <el-tab-pane :label="oLottery.code" :name="oLottery.code" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'KL8' === oLottery.types">
-                  {{ oLottery.name }}
                 </el-tab-pane>
               </E-tabs>
+              <Lottery :code="oLottery.code" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'KL8' === oLottery.types" class="d-inline-block"/>
             </el-tab-pane>
             <el-tab-pane label="六合彩" name="SIX">
-              <E-tabs :type="'card'">
+              <E-tabs>
                 <el-tab-pane :label="oLottery.code" :name="oLottery.code" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'SIX' === oLottery.types">
-                  {{ oLottery.name }}
                 </el-tab-pane>
               </E-tabs>
+              <Lottery :code="oLottery.code" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'SIX' === oLottery.types" class="d-inline-block"/>
+            </el-tab-pane>
+            <el-tab-pane label="幸运28" name="XY28">
+              <E-tabs>
+                <el-tab-pane :label="oLottery.code" :name="oLottery.code" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'XY28' === oLottery.types">
+                </el-tab-pane>
+              </E-tabs>
+              <Lottery :code="oLottery.code" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'XY28' === oLottery.types" class="d-inline-block"/>
             </el-tab-pane>
           </E-tabs>
         </main>
@@ -68,6 +82,7 @@ import Header from '@/Commons/Header/Index.vue'; // @ is an alias to /src
 import NavTop from '@/Commons/NavTop/Index.vue';
 import NavRight from '@/Commons/NavRight/Index.vue';
 import ETabs from '@/Components/ETabs/Index.vue';
+import Lottery from './Lottery/Index.vue';
 
 @Component({
   components: {
@@ -76,6 +91,7 @@ import ETabs from '@/Components/ETabs/Index.vue';
     NavRight,
     Footer,
     ETabs,
+    Lottery,
   },
 })
 class Trend extends Vue {
---
+++ src/Pages/Trend/Lottery/Index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="lottery pt-2 pl-2 pr-2 w-50 align-middle">
+    <span class="icon p-1 d-inline-block align-middle">
+      <img :src="require(`@/assets/images/` + `${code}` + `-sm.png`)"  />
+    </span>
+    <div class="list p-2 d-inline-block w-50 align-middle" >
+      <router-link class="d-inline-block mr-1" :to="{ path: '/trend/information', query: { code: code, tab: 'BASIC' }}">
+        路珠分析
+      </router-link>
+      <router-link class="d-inline-block mr-1" :to="{ path: '/trend/information', query: { code: code, tab: 'BASIC' }}">
+        综合分析
+      </router-link>
+      <router-link class="d-inline-block mr-1" :to="{ path: '/trend/information', query: { code: code, tab: 'BASIC' }}">
+        冷热分析
+      </router-link>
+      <router-link class="d-inline-block mr-1" :to="{ path: '/trend/information', query: { code: code, tab: 'BASIC' }}">
+        单双大小历史
+      </router-link>
+      <router-link class="d-inline-block mr-1" :to="{ path: '/trend/information', query: { code: code, tab: 'BASIC' }}">
+        单双大小历史
+      </router-link>
+      <router-link class="d-inline-block mr-1" :to="{ path: '/trend/information', query: { code: code, tab: 'BASIC' }}">
+        基本走势
+      </router-link>
+      <router-link class="d-inline-block mr-1" :to="{ path: '/trend/information', query: { code: code, tab: 'BASIC' }}">
+        大小单双路珠
+      </router-link>
+      <router-link class="d-inline-block mr-1" :to="{ path: '/trend/information', query: { code: code, tab: 'BASIC' }}">
+        总和路珠
+      </router-link>
+      <router-link class="d-inline-block mr-1" :to="{ path: '/trend/information', query: { code: code, tab: 'BASIC' }}">
+        历史号码统计
+      </router-link>
+      <router-link class="d-inline-block mr-1" :to="{ path: '/trend/information', query: { code: code, tab: 'BASIC' }}">
+        号码路珠
+      </router-link>      
+    </div>
+    <div class="line mt-1">
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+@import 'Index-scoped.scss';
+
+</style>
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+@Component({
+  name: 'Lottery',
+  components: {
+  },
+})
+class Lottery extends Vue {
+  @Prop()
+  public code!: any;
+  public created(): void {
+  }
+
+}
+
+export default Lottery;
+</script>
+
+
+
+
+
+
+
+
+
+
