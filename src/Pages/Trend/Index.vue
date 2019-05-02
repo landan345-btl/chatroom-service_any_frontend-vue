a7c9e719fc6a5315fd079cf3fe50927b89eeb2ee
landan345 <landan345.btl@gmail.com>
2019-05-02 21:47:53 +0800

    /trend css

--- src/Pages/Trend/Index.vue
+++ src/Pages/Trend/Index.vue
@@ -6,9 +6,9 @@
     <el-row :gutter="0">
       <el-col :xs="{span: 22, offset: 1}" :sm="{span: 20, offset: 2}" :md="{span: 18, offset: 3}" :lg="{span: 16, offset: 4}">
         <main class="mt-2 mb-2">
-          <I-tabs :type="'card'">
+          <I-tabs :type="'card'" class="parent">
             <TabPane class="background-white" label="PK拾" name="PK10">
-              <I-tabs>
+              <I-tabs class="son">
                 <TabPane :label="LOTTERIES[oLottery.code].NAME || oLottery.name" :name="oLottery.code" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'PK10' === oLottery.types">
                 </TabPane>
               </I-tabs>
@@ -16,49 +16,49 @@
               <Lottery :lottery="oLottery" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'PK10' === oLottery.types" class="d-inline-block"/>
             </TabPane>
             <TabPane class="background-white" label="时时彩" name="SSC">
-              <I-tabs>
+              <I-tabs class="son">
                 <TabPane :label="LOTTERIES[oLottery.code].NAME || oLottery.name" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'SSC' === oLottery.types">
                 </TabPane>
               </I-tabs>
               <Lottery :lottery="oLottery" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'SSC' === oLottery.types" class="d-inline-block"/>
             </TabPane>
             <TabPane class="background-white" label="11选5" name="11X5">
-              <I-tabs>
+              <I-tabs class="son">
                 <TabPane :label="LOTTERIES[oLottery.code].NAME || oLottery.name" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'11X5' === oLottery.types">
                 </TabPane>
               </I-tabs>
               <Lottery :lottery="oLottery" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'11X5' === oLottery.types" class="d-inline-block"/>
             </TabPane>
             <TabPane class="background-white" label="快三" name="K3">
-              <I-tabs>
+              <I-tabs class="son">
                 <TabPane :label="LOTTERIES[oLottery.code].NAME || oLottery.name" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'K3' === oLottery.types">
                 </TabPane>
               </I-tabs>
               <Lottery :lottery="oLottery" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'K3' === oLottery.types" class="d-inline-block"/>
             </TabPane>
             <TabPane class="background-white" label="快乐十分" name="KLSF">
-              <I-tabs>
+              <I-tabs class="son">
                 <TabPane :label="LOTTERIES[oLottery.code].NAME || oLottery.name" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'KLSF' === oLottery.types">
                 </TabPane>
               </I-tabs>
               <Lottery :lottery="oLottery" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'KLSF' === oLottery.types" class="d-inline-block"/>
             </TabPane>
             <TabPane class="background-white" label="快乐8" name="KL8">
-              <I-tabs>
+              <I-tabs class="son">
                 <TabPane :label="LOTTERIES[oLottery.code].NAME || oLottery.name" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'KL8' === oLottery.types">
                 </TabPane>
               </I-tabs>
               <Lottery :lottery="oLottery" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'KL8' === oLottery.types" class="d-inline-block"/>
             </TabPane>
             <TabPane class="background-white" label="六合彩" name="SIX">
-              <I-tabs>
+              <I-tabs class="son">
                 <TabPane :label="LOTTERIES[oLottery.code].NAME || oLottery.name" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'SIX' === oLottery.types">
                 </TabPane>
               </I-tabs>
               <Lottery :lottery="oLottery" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'SIX' === oLottery.types" class="d-inline-block"/>
             </TabPane>
             <TabPane class="background-white" label="幸运28" name="XY28">
-              <I-tabs>
+              <I-tabs class="son">
                 <TabPane :label="LOTTERIES[oLottery.code].NAME || oLottery.name" v-for="(oLottery, sLotteryId) in getLotteries" :key="sLotteryId" v-if="'XY28' === oLottery.types">
                 </TabPane>
               </I-tabs>
