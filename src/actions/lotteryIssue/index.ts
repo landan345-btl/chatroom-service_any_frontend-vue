839bbdff8d16d28ae5ab8428e4fe69d27e7b98eb
landan345 <landan345.btl@gmail.com>
2019-05-03 14:39:54 +0800

    refactored, action & mutation

--- src/actions/lotteryIssue/index.ts
+++ src/actions/lotteryIssue/index.ts
@@ -5,7 +5,7 @@ let oAxiosHelper = new AxiosHelper();
 
 // VUEX 的 Action，一连串的业务逻辑动作，类似 PHP 的 service 或 Angular 的 service
 let oLotteryIssueAction = {                 // 时间
-  LOTTERY_ISSUE_ACTION_SHOW(oContext: any, oQueries: object, oOptions: object) {
+  LOTTERY_ISSUE_ACTION_SHOW(oContext: any, oQueries: object) {
     let sQueries: string = cObjectQueriesToStringQueries(oQueries);
     let oRequest = {
       path: '/lottery-issue/show' + sQueries,
@@ -15,7 +15,7 @@ let oLotteryIssueAction = {                 // 时间
       return oAxiosHelper.get(oRequest);
     }).then((oResponse) => {
       let aLotteryIssues = oResponse.data.lottery_issues;
-      oContext.commit('LOTTERY_ISSUE_MUTATION_SHOW', aLotteryIssues, oOptions);
+      oContext.commit('LOTTERY_ISSUE_MUTATION_SHOW', aLotteryIssues);
     });
   },
 };
