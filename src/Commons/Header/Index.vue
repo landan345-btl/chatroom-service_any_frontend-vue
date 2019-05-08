55673e62e06c300bb908c47739828ef43db2c805
landan345 <landan345.btl@gmail.com>
2019-05-08 20:25:54 +0800

    fixed: to empty lottery_issues

--- src/Commons/Header/Index.vue
+++ src/Commons/Header/Index.vue
@@ -16,7 +16,9 @@
     </el-row>
     <el-row :gutter="0" class="images pt-1 pb-1">
       <el-col :xs="{span: 22, offset: 1}" :sm="{span: 20, offset: 2}" :md="{span: 18, offset: 3}" :lg="{span: 16, offset: 4}">
-        <a href="/"><img src="@/assets/images/logo-top.png"/></a>
+        <router-link :to="{ path: '/'}">
+          <img src="@/assets/images/logo-top.png"/>
+        </router-link>
         <img class="qrcode" src="@/assets/images/icon-qrcode.png"/>
       </el-col>
     </el-row>
