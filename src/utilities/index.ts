a625aa038e762de770fb0624077fc073460d9d8c
landan345 <landan345.btl@gmail.com>
2019-05-09 13:20:52 +0800

    ultilities/index

---
+++ src/utilities/index.ts
@@ -0,0 +1,29 @@
+import cCount from '@/utilities/count/index';
+import cDetermineSequence from '@/utilities/determineSequence/index';
+import cIsDragonOrTiger from '@/utilities/isDragonOrTiger/index';
+import cIsFishOrShrimpOrCrab from '@/utilities/isFishOrShrimpOrCrab/index';
+import cIsFiveRow from '@/utilities/isFiveRow/index';
+import cIsFrontOrBack from '@/utilities/isFrontOrBack/index';
+import cIsOddOrEven from '@/utilities/isOddOrEven/index';
+import cIsSmallOrLarge from '@/utilities/isSmallOrLarge/index';
+import cObjectQueriesToStringQueries from '@/utilities/objectQueriesToStringQueries/index';
+import cOr from '@/utilities/or/index';
+import cPadStart from '@/utilities/padStart/index';
+import cSubstr from '@/utilities/substr/index';
+import cSum from '@/utilities/sum/index';
+
+export { 
+  cCount as count,
+  cDetermineSequence as determineSequence,
+  cIsDragonOrTiger as isDragonOrTiger,
+  cIsFishOrShrimpOrCrab as isFishOrShrimpOrCrab,
+  cIsFiveRow as isFiveRow,
+  cIsFrontOrBack as isFrontOrBack,
+  cIsOddOrEven as isOddOrEven,
+  cIsSmallOrLarge as isSmallOrLarge,
+  cObjectQueriesToStringQueries as objectQueriesToStringQueries,
+  cOr as or,
+  cPadStart as padStart,
+  cSubstr as substr,
+  cSum as sum,
+};
