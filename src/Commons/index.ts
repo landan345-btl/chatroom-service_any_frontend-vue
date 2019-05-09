d5450a0c0e0076a07eb727edb992bae5eaafe96d
landan345 <landan345.btl@gmail.com>
2019-05-09 16:05:10 +0800

    refactor

--- src/Commons/index.ts
+++ src/Commons/index.ts
@@ -1,11 +1,11 @@
-import _Footer from './Footer/Index.vue';
-import _Header from './Header/Index.vue';
-import _NavRight from './NavRight/Index.vue';
-import _NavTop from './NavTop/Index.vue';
+import Footer from './Footer/Index.vue';
+import Header from './Header/Index.vue';
+import NavRight from './NavRight/Index.vue';
+import NavTop from './NavTop/Index.vue';
 
 export {
-  _Footer as Footer,
-  _Header as Header,
-  _NavRight as NavRight,
-  _NavTop as NavTop,
+  Footer,
+  Header,
+  NavRight,
+  NavTop,
 };
