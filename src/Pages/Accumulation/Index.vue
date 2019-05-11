5e4b0f4641693b3fdf058604d4f43a9a1f59face
landan345 <landan345.btl@gmail.com>
2019-05-11 21:31:39 +0800

    css

--- src/Pages/Accumulation/Index.vue
+++ src/Pages/Accumulation/Index.vue
@@ -4,7 +4,12 @@
     <Nav-top class="z-1001"/>
     <Nav-right/>
     <el-row :gutter="0" class="z-1001">
-      <el-col :xs="{span: 22, offset: 1}" :sm="{span: 20, offset: 2}" :md="{span: 18, offset: 3}" :lg="{span: 16, offset: 4}" class="background-white pb-2 pl-2 pr-2 mt-2 mb-2">
+      <el-col 
+        :xs="{span: 22, offset: 1}" 
+        :sm="{span: 20, offset: 2}" 
+        :md="{span: 18, offset: 3}" 
+        :lg="{span: 16, offset: 4}" 
+        class="background-white pb-2 pl-2 pr-2 mt-2 mb-2">
         <main>
           <div class="top pl-2">
             长龙提醒
