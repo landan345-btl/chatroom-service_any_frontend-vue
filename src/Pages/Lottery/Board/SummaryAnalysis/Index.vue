77d201e8a4f7db3f3bd2de411cbb14985165b5e0
soso383227 <soso383227.btl@gmail.com>
2019-05-13 16:34:47 +0800

    综和分析按钮

--- src/Pages/Lottery/Board/SummaryAnalysis/Index.vue
+++ src/Pages/Lottery/Board/SummaryAnalysis/Index.vue
@@ -2,127 +2,121 @@
   <div class="summary-analysis">
     <div class="top p-1 font-weight-bold">
       <span class="title">综合分析</span>
-      <span @click="toggleBorder(1)" :class=" changeBorder.indexOf( 1 ) !== -1 ? 'border-orange':'' ">
-        <span>今日双面统计/号码统计</span>
-        <span class="background-image" v-show="changeBorder.indexOf( 1 ) !== -1"></span>
-      </span> 
-      <span @click="toggleBorder(2)" :class="  changeBorder.indexOf( 2 ) !== -1 ? 'border-orange':'' ">
-        <span>长龙统计</span>
-        <span class="background-image" v-show="changeBorder.indexOf( 2 ) !== -1"></span>
-      </span> 
-      <span @click="toggleBorder(3)" :class="  changeBorder.indexOf( 3 ) !== -1 ? 'border-orange':'' ">
-        <span>号码分析</span>
-        <span class="background-image" v-show="changeBorder.indexOf( 3 ) !== -1"></span>
-      </span> 
+        <I-button class="ml-2" v-on:handle-click="toggleTodayTwoSideNumberStatics()" v-if="['11X5','SSC' , 'PK10'].includes(types.toUpperCase())">今日双面/号码统计</I-button>
+        <I-button class="ml-2" v-on:handle-click="toggleAccumulationRemider()" v-if="['11X5','SSC' , 'PK10'].includes(types.toUpperCase())">长龙提醒</I-button>
+        <I-button class="ml-2" v-on:handle-click="toggleNumberAnalysis()" v-if="['11X5','SSC' , 'PK10'].includes(types.toUpperCase())">号码分析</I-button>
     </div>
-
-    <div class="p-2 double-sided-statistics" v-show=" changeBorder.indexOf( 1 ) !== -1 ">
-      <div> 
-        <div class="font-size-2">今日号码统计</div>
-        <table>
-          <tr>
-            <td>号码</td>
-            <td>0</td>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
-            <td>8</td>
-            <td>9</td>
-          </tr>
-          <tr>
-            <td>出现次数</td>
-            <td>{{ numberFrequency( lotteryIssues , 0 ) }}</td>
-            <td>{{ numberFrequency( lotteryIssues , 1 ) }}</td>
-            <td>{{ numberFrequency( lotteryIssues , 2 ) }}</td>
-            <td>{{ numberFrequency( lotteryIssues , 3 ) }}</td>
-            <td>{{ numberFrequency( lotteryIssues , 4 ) }}</td>
-            <td>{{ numberFrequency( lotteryIssues , 5 ) }}</td>
-            <td>{{ numberFrequency( lotteryIssues , 6 ) }}</td>
-            <td>{{ numberFrequency( lotteryIssues , 7 ) }}</td>
-            <td>{{ numberFrequency( lotteryIssues , 8 ) }}</td>
-            <td>{{ numberFrequency( lotteryIssues , 9 ) }}</td>
-          </tr>
-        </table>
-        <table>
-          <tr>
-            <td rowspan="2"> <span>球次</span> </td>
-            <td colspan="4">总和</td>
-            <td colspan="4">第一球</td>
-            <td colspan="4">第二球</td>
-            <td colspan="4">第三球</td>
-            <td colspan="4">第四球</td>
-            <td colspan="4">第五球</td>
-          </tr>
-          <tr>
-            <td>单</td>
-            <td>双</td>
-            <td>大</td>
-            <td>小</td>
-            <td>单</td>
-            <td>双</td>
-            <td>大</td>
-            <td>小</td>
-            <td>单</td>
-            <td>双</td>
-            <td>大</td>
-            <td>小</td>
-            <td>单</td>
-            <td>双</td>
-            <td>大</td>
-            <td>小</td>
-            <td>单</td>
-            <td>双</td>
-            <td>大</td>
-            <td>小</td>
-            <td>单</td>
-            <td>双</td>
-            <td>大</td>
-            <td>小</td>
-          </tr>
-          <tr>
-            <td>出现次数</td>
-            <td>{{ he( todayTwoSideRecords , 'small' ) }}</td>
-            <td>{{ he( todayTwoSideRecords , 'large' ) }}</td>
-            <td>{{ he( todayTwoSideRecords , 'odd' ) }}</td>
-            <td>{{ he( todayTwoSideRecords , 'even' ) }}</td>
-            <td v-for=" ( iOne , i ) in  todayTwoSideRecords[0]" :key="i">{{ iOne }}</td>
-            <td v-for=" ( iOne , i ) in  todayTwoSideRecords[1]" :key="i">{{ iOne }}</td>
-            <td v-for=" ( iOne , i ) in  todayTwoSideRecords[2]" :key="i">{{ iOne }}</td>
-            <td v-for=" ( iOne , i ) in  todayTwoSideRecords[3]" :key="i">{{ iOne }}</td>
-            <td v-for=" ( iOne , i ) in  todayTwoSideRecords[4]" :key="i">{{ iOne }}</td>
-          </tr>
-        </table>
-      </div>
-    </div>
-
-    <div class="p-2 " v-show=" changeBorder.indexOf( 2 ) !== -1 ">
-      <div class="long-reminder">
-        <div>长龙连开提醒</div>
-        <div>
-          <div>第二球: 小  5  期</div>
-          <div>第二球: 小  5  期</div>
-          <div>第二球: 小  5  期</div>
-          <div>第二球: 小  5  期</div>
-          <div>第二球: 小  5  期</div>
-          <div>第二球: 小  5  期</div>
-          <div>第二球: 小  5  期</div>
+    <transition name="slide-fade">
+      <div class="p-2 double-sided-statistics" v-show="isTodayTwoSideNumberStaticShowed ">
+        <div> 
+          <div class="font-size-2">今日号码统计</div>
+          <table>
+            <tr>
+              <td>号码</td>
+              <td>0</td>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+              <td>4</td>
+              <td>5</td>
+              <td>6</td>
+              <td>7</td>
+              <td>8</td>
+              <td>9</td>
+            </tr>
+            <tr>
+              <td>出现次数</td>
+              <td>{{ numberFrequency( lotteryIssues , 0 ) }}</td>
+              <td>{{ numberFrequency( lotteryIssues , 1 ) }}</td>
+              <td>{{ numberFrequency( lotteryIssues , 2 ) }}</td>
+              <td>{{ numberFrequency( lotteryIssues , 3 ) }}</td>
+              <td>{{ numberFrequency( lotteryIssues , 4 ) }}</td>
+              <td>{{ numberFrequency( lotteryIssues , 5 ) }}</td>
+              <td>{{ numberFrequency( lotteryIssues , 6 ) }}</td>
+              <td>{{ numberFrequency( lotteryIssues , 7 ) }}</td>
+              <td>{{ numberFrequency( lotteryIssues , 8 ) }}</td>
+              <td>{{ numberFrequency( lotteryIssues , 9 ) }}</td>
+            </tr>
+          </table>
+          <table>
+            <tr>
+              <td rowspan="2"> <span>球次</span> </td>
+              <td colspan="4">总和</td>
+              <td colspan="4">第一球</td>
+              <td colspan="4">第二球</td>
+              <td colspan="4">第三球</td>
+              <td colspan="4">第四球</td>
+              <td colspan="4">第五球</td>
+            </tr>
+            <tr>
+              <td>单</td>
+              <td>双</td>
+              <td>大</td>
+              <td>小</td>
+              <td>单</td>
+              <td>双</td>
+              <td>大</td>
+              <td>小</td>
+              <td>单</td>
+              <td>双</td>
+              <td>大</td>
+              <td>小</td>
+              <td>单</td>
+              <td>双</td>
+              <td>大</td>
+              <td>小</td>
+              <td>单</td>
+              <td>双</td>
+              <td>大</td>
+              <td>小</td>
+              <td>单</td>
+              <td>双</td>
+              <td>大</td>
+              <td>小</td>
+            </tr>
+            <tr>
+              <td>出现次数</td>
+              <td>{{ he( todayTwoSideRecords , 'small' ) }}</td>
+              <td>{{ he( todayTwoSideRecords , 'large' ) }}</td>
+              <td>{{ he( todayTwoSideRecords , 'odd' ) }}</td>
+              <td>{{ he( todayTwoSideRecords , 'even' ) }}</td>
+              <td v-for=" ( iOne , i ) in  todayTwoSideRecords[0]" :key="i">{{ iOne }}</td>
+              <td v-for=" ( iOne , i ) in  todayTwoSideRecords[1]" :key="i">{{ iOne }}</td>
+              <td v-for=" ( iOne , i ) in  todayTwoSideRecords[2]" :key="i">{{ iOne }}</td>
+              <td v-for=" ( iOne , i ) in  todayTwoSideRecords[3]" :key="i">{{ iOne }}</td>
+              <td v-for=" ( iOne , i ) in  todayTwoSideRecords[4]" :key="i">{{ iOne }}</td>
+            </tr>
+          </table>
         </div>
       </div>
-    </div>
-
-    <div class="p-2" v-show=" changeBorder.indexOf( 3 ) !== -1 ">
-      <TodayTwoSideNumberStatics 
-        :types="types" 
-        :onNumbers="onNumbers" 
-        :onOddOrEvenOrSmallOrLargeOrPairRedcords="onOddOrEvenOrSmallOrLargeOrPairRedcords"
-        @handle-toggle-number="toggleNumber" 
-        @handle-toggle-oddoreven="toggleOddOrEven"
-        />
-    </div>
+    </transition>
+    <transition name="slide-fade">
+      <div class="p-2 " v-show="isAccumulationRemiderShowed">
+        <div class="long-reminder">
+          <div>长龙连开提醒</div>
+          <div>
+            <div>第二球: 小  5  期</div>
+            <div>第二球: 小  5  期</div>
+            <div>第二球: 小  5  期</div>
+            <div>第二球: 小  5  期</div>
+            <div>第二球: 小  5  期</div>
+            <div>第二球: 小  5  期</div>
+            <div>第二球: 小  5  期</div>
+          </div>
+        </div>
+      </div>
+    </transition>
+    <transition name="slide-fade">
+      <div class="p-2" v-show="isNumberAnalysisShowed">
+        <TodayTwoSideNumberStatics 
+          :types="types" 
+          :onNumbers="onNumbers" 
+          :onOddOrEvenOrSmallOrLargeOrPairRedcords="onOddOrEvenOrSmallOrLargeOrPairRedcords"
+          @handle-toggle-number="toggleNumber" 
+          @handle-toggle-oddoreven="toggleOddOrEven"
+          />
+      </div>
+    </transition>
 
     <div class="p-2">
       <div>
@@ -174,6 +168,7 @@
 <script lang="ts">
 import { Component, Vue , Prop } from 'vue-property-decorator';
 import LOTTERIES from '@/CONFIGS/LOTTERIES/index';
+import IButton from '@/Components/IButton/Index.vue';
 import Numbers from '@/Components/Numbers/Index.vue';
 import TodayTwoSideNumberStatics from '@/Pages/Lottery/Board/LotteryIssue/NumberAnalysis/Index.vue';
 // TODO
@@ -185,10 +180,16 @@ import TodayTwoSideNumberStatics from '@/Pages/Lottery/Board/LotteryIssue/Number
   components: {
     Numbers,
     TodayTwoSideNumberStatics,
+    IButton,
   },
 })
 class SummaryAnalysis extends Vue {
   public changeBorder: any = [];
+
+  public isTodayTwoSideNumberStaticShowed: boolean = false;
+  public isAccumulationRemiderShowed: boolean = false;
+  public isNumberAnalysisShowed: boolean = false;
+
   public onNumbers: {
     [key: string]: boolean,
   } = {
@@ -231,12 +232,16 @@ class SummaryAnalysis extends Vue {
   @Prop()
   public types!: any;
 
-  public toggleBorder( iNumber: number ) {
-    if ( this.changeBorder.indexOf( iNumber ) !== -1 ) {
-      this.changeBorder.splice( this.changeBorder.indexOf( iNumber ) , 1 );
-      return;
-    }
-    this.changeBorder.push( iNumber );
+  public toggleTodayTwoSideNumberStatics(): void {
+    this.isTodayTwoSideNumberStaticShowed = !this.isTodayTwoSideNumberStaticShowed;
+  }
+
+  public toggleAccumulationRemider(): void {
+    this.isAccumulationRemiderShowed = !this.isAccumulationRemiderShowed;
+  }
+
+  public toggleNumberAnalysis(): void {
+    this.isNumberAnalysisShowed = !this.isNumberAnalysisShowed;
   }
 
   public he( oHe: any , s: any ) { // 总和单双大小
