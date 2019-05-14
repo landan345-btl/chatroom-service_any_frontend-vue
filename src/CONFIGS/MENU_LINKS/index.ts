3c29b0c75963e7cf5811d655fd1e3badd590da4c
landan345 <landan345.btl@gmail.com>
2019-05-14 14:37:18 +0800

    css

---
+++ src/CONFIGS/MENU_LINKS/index.ts
@@ -0,0 +1,38 @@
+const MENU_LINKS = [
+  {
+    NAME: '首页',
+    LINK: 'http://service.lottery.ques98.cn/home',
+    ICON: null,
+    SUB_MENU_LINKS: []
+  },
+  {
+    NAME: '彩票大厅',
+    LINK: null,
+    ICON: null,
+    SUB_MENU_LINKS: [
+      {
+        NAME: '热门彩',
+        LINK: '',
+      },
+      {
+        NAME: '热门彩',
+        LINK: '',
+      }
+
+    ]
+  },
+  {
+    NAME: '长龙提醒',
+    LINK: 'http://service.lottery.ques98.cn/accumulation',
+    ICON: null,
+    SUB_MENU_LINKS: []
+  },
+  {
+    NAME: '走势图表',
+    LINK: 'http://service.lottery.ques98.cn/trend',
+    ICON: null,
+    SUB_MENU_LINKS: []
+  },
+];
+
+export default MENU_LINKS;
