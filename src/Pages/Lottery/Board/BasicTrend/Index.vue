d76cdfe4c3ef6a8f1b6d89127127d8324e83e8e9
landan345 <landan345.btl@gmail.com>
2019-05-14 19:03:17 +0800

    refactor

--- src/Pages/Lottery/Board/BasicTrend/Index.vue
+++ src/Pages/Lottery/Board/BasicTrend/Index.vue
@@ -432,8 +432,14 @@
 
 </style>
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import ITabs from '@/Components/ITabs/Index.vue';
+import {
+  Component,
+  Vue,
+} from 'vue-property-decorator';
+
+import {
+  ITabs,
+} from '@/Components/';
 
 import {
   LOTTERIES,
