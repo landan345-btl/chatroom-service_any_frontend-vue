5c30fed30aeb430655d90a4ca2a606ae8ae4e24a
landan345 <landan345.btl@gmail.com>
2019-05-14 18:48:12 +0800

    css

--- src/Pages/Lottery/Board/TodayNumberStatics/Index.vue
+++ src/Pages/Lottery/Board/TodayNumberStatics/Index.vue
@@ -94,9 +94,14 @@
 
 </style>
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { 
+  Component,
+  Vue,
+} from 'vue-property-decorator';
 
-import ITabs from '@/Components/ITabs/Index.vue';
+import {
+  ITabs,
+} from '@/Components/';
 
 import {
   LOTTERIES,
