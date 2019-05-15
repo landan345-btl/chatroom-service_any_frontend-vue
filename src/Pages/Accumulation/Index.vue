c1a6c561edad6e64a76b62eee6e36fcc5f73ca97
soso383227 <soso383227.btl@gmail.com>
2019-05-15 17:27:45 +0800

    长龙  改

--- src/Pages/Accumulation/Index.vue
+++ src/Pages/Accumulation/Index.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="page accumulation">
-    <Header />
-    <Nav-top class="z-1000"/>
-    <Nav-right style="z-index:1002"/>
-    <Row :gutter="0" class="z-1000 backgroung-gray">
+    <Header /> 
+    <Nav-top />
+    <Nav-right style="z-index:1001"/>
+    <Row :gutter="0" class=" backgroung-gray">
       <Col 
         :xs="{span: 22, offset: 1}" 
         :sm="{span: 20, offset: 2}" 
         :md="{span: 20, offset: 2}" 
         :lg="{span: 20, offset: 2}"
         :xl="{span: 16, offset: 4}"
-        class="background-white pb-2 pr-1 mt-2 mb-2">
+        class="background-white pb-2 pr-1 mt-2 mb-2" style="z-index:1002">
         <main>
           <div class="top mt-2 ml-2 font-size-2p5">
             长龙提醒
           </div>
           <div class="line mt-1 mb-2">
           </div>
-          <Toolbar class="ml-2"/>
+          <Toolbar class="ml-2" :lotteries="lotteries"/>
           <Board />
         </main>
       </Col>
@@ -56,7 +56,10 @@ import Board from './Board/Index.vue';
   },
 })
 class Accumulation extends Vue {
-
+  public get lotteries(): object {
+    let oLotteries: any = this.$store.state.lotteries;
+    return oLotteries;
+  }
 }
 
 export default Accumulation;
