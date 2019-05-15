d73b246cce0354dbb510e8744c4fd8b4c7b9de55
landan345 <landan345.btl@gmail.com>
2019-05-15 16:07:48 +0800

    fixed

---
+++ src/Components/ICollapse/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <Collapse simple>
+    <slot></slot>
+  </Collapse>
+</template>
+
+<style scoped lang="scss">
+@import 'Index-scoped.scss';
+
+</style>
+
+<script lang="ts">
+import {
+  Component,
+  Prop,
+  Vue,
+} from 'vue-property-decorator';
+
+@Component({
+  name: 'ICollapse',
+})
+class ICollapse extends Vue {
+  @Prop()
+  public name!: string;
+}
+export default ICollapse;
+
+</script>
