d0dee7b837fa5d60bc1e76d2524bc3f2e9a4ec74
soso383227 <soso383227.btl@gmail.com>
2019-05-18 18:56:15 +0800

    Fixed space

--- src/Pages/Lottery/Board/LotteryIssue/Index.vue
+++ src/Pages/Lottery/Board/LotteryIssue/Index.vue
@@ -2,13 +2,13 @@
   <div class="lottery-issue">
     <div class="top pl-2 pt-1 pb-1 font-weight-bold">
       <span class="font-size-2">即时开奖</span>
-      <I-button class="ml-2" v-on:handle-click="toggleTodayTwoSideNumberStatics()" v-if="types && ['11X5','SSC' , 'PK10'].includes(types.toUpperCase())">今日双面/号码统计</I-button>
-      <I-button class="ml-2" v-on:handle-click="toggleAccumulationRemider()" v-if="types && ['11X5','SSC' , 'PK10'].includes(types.toUpperCase())">长龙提醒</I-button>
-      <I-button class="ml-2" v-on:handle-click="toggleNumberAnalysis()" v-if="types && ['11X5','SSC' , 'PK10'].includes(types.toUpperCase())">号码分析</I-button>
+      <I-button class="ml-2" v-on:handle-click="toggleTodayTwoSideNumberStatics()" v-if="types && ['11X5', 'SSC', 'PK10'].includes(types.toUpperCase())">今日双面/号码统计</I-button>
+      <I-button class="ml-2" v-on:handle-click="toggleAccumulationRemider()" v-if="types && ['11X5', 'SSC', 'PK10'].includes(types.toUpperCase())">长龙提醒</I-button>
+      <I-button class="ml-2" v-on:handle-click="toggleNumberAnalysis()" v-if="types && ['11X5', 'SSC', 'PK10'].includes(types.toUpperCase())">号码分析</I-button>
     </div>
     <I-divider/>
     <transition name="slide-fade">
-      <Today-two-side-number-statics 
+      <Today-two-side-number-statics
         :types="types" 
         :lotteryIssues="lotteryIssues"
         :todayTwoSideRecords="todayTwoSideRecords"
