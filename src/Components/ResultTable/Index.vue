28df03451ce62b0e119c32a8549771cdfd05f102
soso383227 <soso383227.btl@gmail.com>
2019-05-29 22:06:20 +0800

    navTop  and  路珠数据渲染

--- src/Components/ResultTable/Index.vue
+++ src/Components/ResultTable/Index.vue
@@ -1,41 +1,40 @@
 <template>
   <div class="result-table ml-2 mr-2 font-size-1p5 background-white">
-    <div class="pl-1 pt-1 pb-1 table-top">
-      <span class="mr-1">连续出现次数</span>
-      <input class="mr-1" type="number" min="0" placeholder="1"/>
-      <span  class="mr-1">次以上</span>
-      <select class="mr-1">
-        <option value="大">大</option>
-        <option value="小">小</option>
-      </select>
-      <Button type="primary" size="small">确定</Button>
-      <span>
-        出现的次数为：
-        <span>0</span>
-      </span>
-      <span class="float-right m-1 grand">
-        <span>今天</span>
-        <span> 累计：单(</span>
-        <!-- <span v-for="(item , i) in oDragonOrTigerCount" :key="i"> {{oddOrEvenOrSmallOrLargeOrDragonOrTiger(i)}}({{item}})</span> -->
-        <span>123</span>
-        <span>)&nbsp;&nbsp;双(</span>
-        <span>456</span>)
-        <span class="color-red">&nbsp;&nbsp;冠军</span>&nbsp;&nbsp;
-        <span class="color-red">龙虎</span>&nbsp;&nbsp;
-        <span class="color-red">最新</span>&nbsp;&nbsp;
-        <span class="color-red"><Icon type="md-arrow-down" /></span>
-      </span>
-    </div>
-    <div class="table-con w-100" dir=rtl>
-      <table class="faloat-right text-center">
-        <tbody>
-          <tr>
-            <td v-for="(aOddEvenOrSmallOrLarge, iIndex ) in oddEvenOrSmallOrLargeStrings" :key="iIndex" :class="iIndex % 2 === 1 ? 'background-even':''" >
-              <p v-for="(iCount, index) in aOddEvenOrSmallOrLarge" :key="index">{{ iCount }}</p>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <div>
+      <div class="pl-1 pt-1 pb-1 table-top">
+        <span class="mr-1">连续出现次数</span>
+        <input class="mr-1" type="number" min="0" placeholder="1"/>
+        <span  class="mr-1">次以上</span>
+        <select class="mr-1">
+          <option value="大">大</option>
+          <option value="小">小</option>
+        </select>
+        <Button type="primary" size="small">确定</Button>
+        <span>
+          出现的次数为：
+          <span>0</span>
+        </span>
+        <span class="float-right m-1 grand">
+          <span>今天</span>
+          <span>累计:</span>
+          <span v-for="(count , iIndex) in oDragonOrTigerCount" :key="iIndex"> {{oddOrEvenOrSmallOrLargeOrDragonOrTiger(iIndex)}}({{count}})</span>
+          <span class="color-red">&nbsp;&nbsp;冠军</span>&nbsp;&nbsp;
+          <span class="color-red">龙虎</span>&nbsp;&nbsp;
+          <span class="color-red">最新</span>&nbsp;&nbsp;
+          <span class="color-red"><Icon type="md-arrow-down" /></span>
+        </span>
+      </div>
+      <div class="table-con w-100" dir=rtl>
+        <table class="faloat-right text-center">
+          <tbody>
+            <tr>
+              <td v-for="(aOddEvenOrSmallOrLarge, iIndex ) in oddEvenOrSmallOrLargeStrings" :key="iIndex" :class="iIndex % 2 === 1 ? 'background-even':''" >
+                <p v-for="(iCount, index) in aOddEvenOrSmallOrLarge" :key="index">{{ iCount }}</p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
@@ -56,23 +55,30 @@ class ResultTable extends Vue {
   public resultOddOrEvensAndSmallOrLarges!: any;
 
   @Prop()
-  public oddOrEvensAndSmallOrLargeCount!: any;
+  public rank: any;
 
   @Prop()
-  public dragonOrTigerResult!: any;
+  public oddEvenOrSmallOrLarge!: any;
 
   @Prop()
-  public dragonOrTigerCount!: any;
-
-  @Prop()
-  public firstAndSecondSummation!: any;
-  
-  @Prop()
-  public firstAndSecondSummationCount!: any;
+  public oDragonOrTigerCount!: any;
 
   public oddEvenOrSmallOrLargeStrings =[
-    ['双','双'],['单','单','单'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双'],['双','双','双']
+    ['双','双'],['单','单','单'],['双','双','双','双','双','双','双','双','双'],['双','双','双'],['双','双'],['单','单','单'],['双','双','双'],['双','双','双'],['双','双'],['单','单','单'],['双','双','双'],['双','双','双'],['单','单','单'],['双','双','双'],['双','双','双'],['单','单','单'],['双','双','双'],['双','双','双'],['单','单','单'],['双','双','双'],['双','双','双'],['单','单','单'],['双','双','双'],['双','双','双'],['单','单','单'],['双','双','双'],['双','双','双'],['单','单','单'],['双','双','双'],['双','双','双'],['单','单','单'],['双','双','双'],['双','双','双','双','双','双','双','双','双'],
   ];
+
+  public get isoddEvenOrSmallOrLarge() {
+    let aOddEvenOrSmallOrLarge: any = {
+      smallOrLarge: '大小',
+      oddOrEven: '单双',
+      odd: '单双',
+      small: '大小',
+      dragon: '龙虎',
+      guanyaodd: '单双',
+      guanyasmall: '大小',
+    };
+    return aOddEvenOrSmallOrLarge[this.oddEvenOrSmallOrLarge];
+  }
 
   public oddOrEvenOrSmallOrLargeOrDragonOrTiger(sKey: string) {
     let oResult: any = {
