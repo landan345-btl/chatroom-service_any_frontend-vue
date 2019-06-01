ee590ab0802b16b2718e0f2a504a92cad3cec659
landan345 <landan345.btl@gmail.com>
2019-06-01 14:52:22 +0800

    CONFIGS

--- src/CONFIGS/BACKEND/index.ts
+++ src/CONFIGS/BACKEND/index.ts
@@ -1,5 +1,5 @@
 const BACKEND = {
-  BASE_URL: process.env.BACKEND_BASE_URL || 'http://dev.api.lottery.ques98.cn/service/resource',
+  HOST: process.env.BACKEND_BASE_URL || 'http://dev.api.lottery.ques98.cn/service/resource',
   INTERVAL_TIME: 10 * 1000,
   RETRIED_TIMES: 5,
 };
