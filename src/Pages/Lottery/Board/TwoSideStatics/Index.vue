d472e1ecf3e7d9033a0830721f36f443d9fce7fc
soso383227 <soso383227.btl@gmail.com>
2019-06-01 16:31:41 +0800

    修 改 模式选择 按钮

--- src/Pages/Lottery/Board/TwoSideStatics/Index.vue
+++ src/Pages/Lottery/Board/TwoSideStatics/Index.vue
@@ -4,31 +4,33 @@
       <span class="font-size-2 line-height3">两面统计</span>
     </div>
     <I-divider/>
-    <div class="explain">
+    <div class="explain w-100 ml-2 mr-2 mt-2">
       <span>说明：</span>
       <span>以每10期为一组，统计最近20组中单双大小的出现次数。</span>
     </div>
-    <div class="ball-number">
+    <div class="pl-2 pt-1 pb-1">
       <I-radio-group @on-change="checkRank = $event" :value="checkRank" type="button" class="line-height3">
        <Radio :label="sKey" v-for="(rank, sKey) in texts[ types ]" :key="sKey">{{ rank }}</Radio>
       </I-radio-group>
     </div>
-    <table>
-      <tr>
-        <td class="w-30">日期</td>
-        <td>单</td>
-        <td>双</td>
-        <td>大</td>
-        <td>小</td>
-      </tr>
-      <tr v-for="(item ,i) in 15" :key="i">
-        <td>05月08日&nbsp;&nbsp;&nbsp;20190508033 - 20190508042期</td>
-        <td>4</td>
-        <td>4</td>
-        <td>4</td>
-        <td>4</td>
-      </tr>
-    </table>
+    <div class="pb-2 pl-2 pr-2 two-side-table">
+      <table class="w-100 text-center">
+        <tr>
+          <td class="w-30 font-size-2" >日期</td>
+          <td class="font-size-2">单</td>
+          <td class="font-size-2">双</td>
+          <td class="font-size-2">大</td>
+          <td class="font-size-2">小</td>
+        </tr>
+        <tr v-for="(item ,i) in 15" :key="i">
+          <td>05月08日&nbsp;&nbsp;&nbsp;<span>20190508033</span> - <span>20190508042</span>期</td>
+          <td>4</td>
+          <td>4</td>
+          <td>4</td>
+          <td>4</td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 <style scoped lang="scss">
@@ -63,12 +65,14 @@ import LOTTERIES from '@/CONFIGS/LOTTERIES/index';
 class TwoSideStatics extends Vue {
   @Prop()
   public types!: any;
-  
+
   public texts = {
     PK10: ['冠军', '亚军', '第三名', '第四名', '第五名', '第六名', '第七名', '第八名', '第九名', '第十名', ],
     SSC: [ '第一球', '第二球', '第三球', '第四球', '第五球', ],
   };  
-  public position: any = '第一球';
+
+  public checkRank: number = 0;
+
 }
 
 export default TwoSideStatics;
