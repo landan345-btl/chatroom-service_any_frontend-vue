13f551c57057f5f19046db102099513b261a7a48
landan345 <landan345.btl@gmail.com>
2019-06-21 14:34:14 +0800

    PORT

--- src/CONFIGS/SOCKET/INDEX.ts
+++ src/CONFIGS/SOCKET/INDEX.ts
@@ -1,9 +1,6 @@
-import dotenv from 'dotenv';
-dotenv.config();
-console.log(process.env);
 
 const SOCKET: any = {
-  HOST: process.env.SOCKET_HOST || "http://fea.socket.chatroom.ques98.cn",
+  HOST: process.env.VUE_APP_SOCKET_HOST || "http://fea.socket.chatroom.ques98.cn",
   PORT: "",
 };
 
