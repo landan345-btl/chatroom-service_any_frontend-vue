96fb735e565fabe3b030b28ba218f8bcfa2fc583
landan345 <landan345.btl@gmail.com>
2019-07-08 16:15:22 +0800

    fixed

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app" @click="closeMenu">
-    <!-- <x-nav v-show=""></x-nav> -->
     <div class="wrap-router">
       <transition name="viewTransition">
         <keep-alive include="chat">
@@ -10,12 +9,6 @@
     </div>
     <header class="nav v-transfer-dom">
       <div class="nav-item">
-        <!-- <div class="nav-left-wrap">
-          <div class="back" @click="back">
-            <a href="javascript:void(0)" class="back-btn">首页</a>
-          </div>
-          <div class="left-slot"></div>
-        </div> -->
       </div>
       <div class="title" @click="goToChatroom()" style="text-align:center;">聊天室</div>
       <div class="nav-item" style="width:224px;">
@@ -28,11 +21,6 @@
           >
             <i class="iconfont icon-icon-" style="font-size: 1em;"></i>
           </a>
-          <!-- <div class="drawer">
-            <a href="javascript:;" @click="taggle">
-              <span class="iconfont icon-guidedrawer"></span>
-            </a>
-          </div> -->
         </div>
       </div>
     </header>
